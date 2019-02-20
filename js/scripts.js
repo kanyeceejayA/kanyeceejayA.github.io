d10efe01b4e2c43f3657da57cd699f32bebc7267
kanyeceejayA <33685273+kanyeceejayA@users.noreply.github.com>
2019-02-21 01:12:15 +0300

    changes to content and scroll speed

--- js/scripts.js
+++ js/scripts.js
@@ -16,12 +16,16 @@
         var heading = $(this).attr('href');
         var scrollDistance = $(heading).offset().top;
 
+        // $('html, body').animate({
+        //     scrollTop: scrollDistance + 'px'
+        // }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+
         $('html, body').animate({
             scrollTop: scrollDistance + 'px'
-        }, Math.abs(window.pageYOffset - $(heading).offset().top) / 1);
+        }, 300);
 
         // Hide the menu once clicked if mobile
-        if ($('header').hasClass('active')) {
+        if ($('header').hasClass('active')) {offset
             $('header, body').removeClass('active');
         }
     });
